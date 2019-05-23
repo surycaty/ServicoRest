31ead8c00ea795832affa0d681c4a21a956e3fff
Adriano Surycaty <surycaty.ibg@gmail.com>
2019-05-22 21:19:33 -0300

    Corrigindo controle de acesso

--- src/main/java/br/com/dbc/application/ProvaApplication.java
+++ src/main/java/br/com/dbc/application/ProvaApplication.java
@@ -2,8 +2,10 @@ package br.com.dbc.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "br.com.dbc.application")
 public class ProvaApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/dbc/application/controller/ClienteController.java
+++ src/main/java/br/com/dbc/application/controller/ClienteController.java
@@ -3,6 +3,7 @@ package br.com.dbc.application.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import br.com.dbc.application.to.EmprestimoTO;
 import br.com.dbc.application.util.Resultado;
 import br.com.dbc.application.util.Util;
 
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping("/cliente")
 public class ClienteController {
@@ -28,18 +30,6 @@ public class ClienteController {
 
 	@Autowired
 	private Resultado resultado;
-
-//		/**
-//		 * Chama a view (simulador.jsp) para editar um registro cadastrado
-//		 * 
-//		 */
-//		@RequestMapping(value = "/simulador.html/{codigo}", method = RequestMethod.GET)
-//		public ModelAndView simulador(@PathVariable int codigo) {
-//
-//			Cliente cliente = repository.consultarPorCodigo(codigo);
-//			
-//			return new ModelAndView("simulador", "cliente", cliente);
-//		}
 
 	/**
 	 * Consultar todos os registros cadastrados
